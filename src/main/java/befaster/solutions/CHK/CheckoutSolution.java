16ba9b8b91b21064faf1a7b698444318dff1c474
sbx_user1059 <sbx_user1059@ip-10-128-56-59.eu-west-2.compute.internal>
2019-02-17 17:47:55 +0000

    Sun Feb 17 17:47:55 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,9 +1,23 @@
 package befaster.solutions.CHK;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import befaster.runner.SolutionNotImplementedException;
 
 public class CheckoutSolution {
-    public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
-    }
+
+	private final Map<Character, Integer> map = new HashMap<>();
+
+	public CheckoutSolution() {
+		map.put('A', 50);
+		map.put('B', 30);
+		map.put('C', 20);
+		map.put('D', 15);
+	}
+
+	public Integer checkout(final String skus) {
+		throw new SolutionNotImplementedException();
+	}
 }
+
