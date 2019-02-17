b20da3e6b4ed3ca869dafd32bce81c1c7dd592ee
sbx_user1059 <sbx_user1059@ip-10-128-56-59.eu-west-2.compute.internal>
2019-02-17 17:50:55 +0000

    Sun Feb 17 17:50:55 UTC 2019

---
+++ src/main/java/befaster/solutions/CHK/Offer.java
@@ -0,0 +1,23 @@
+package befaster.solutions.CHK;
+
+public class Offer {
+
+	private Character sku;
+	private int price;
+
+	public Character getSku() {
+		return sku;
+	}
+
+	public void setSku(final Character sku) {
+		this.sku = sku;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(final int price) {
+		this.price = price;
+	}
+}
